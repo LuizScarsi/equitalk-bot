eccc268adfbeeb37868694461c1748003bec0572
luizscarsi <luizaugustoscarsi@gmail.com>
2023-12-09 14:59:54 -0300

    feat: Import handlers into router

--- bot-lex-backend/router.js
+++ bot-lex-backend/router.js
@@ -1,9 +1,15 @@
 const { INVALID_FORMAT } = require("./core/config")
 const { textToSpeech } = require("./handlers/textToSpeech")
+const { speechToText } = require("./handlers/speechToText")
+const { imageToText } = require("./handlers/imageToText")
+const { imageToSpeech } = require("./handlers/imageToSpeech")
 const { prepareResponse } = require("./helper/helper")
 
 const intentHandlers = {
-    "Text-to-Speech": textToSpeech
+    "Text-to-Speech": textToSpeech,
+    "Speech-to-Text": speechToText,
+    "Image-to-Text": imageToText,
+    "Image-to-Speech": imageToSpeech
 }
 
 const handler = async (event, context) => {
