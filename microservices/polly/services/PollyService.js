48bc7bf2699de4e88c1d24dc04bf197f644b242a
luizscarsi <luizaugustoscarsi@gmail.com>
2023-12-11 11:16:18 -0300

    feat: Add polly SpeechSynthesis service

--- microservices/polly/services/PollyService.js
+++ microservices/polly/services/PollyService.js
@@ -19,5 +19,36 @@ class PollyService {
             LanguageCode: "pt-br",
             OutputS3BucketName: BUCKET_NAME,
         }
+        
+        try {
+            const command = new StartSpeechSynthesisTaskCommand(speechParams)
+            const data = await this.polly.send(command)
+            const taskId = data.SynthesisTask.TaskId
+            console.log(("Task ID:", taskId))
+            while (true) {
+                const getSpeechSynthesisTaskParams = {
+                    TaskId: taskId,
+                }
+                const getSpeechSynthesisTaskCommand = new GetSpeechSynthesisTaskCommand(getSpeechSynthesisTaskParams)
+                const dataTask = await this.polly.send(getSpeechSynthesisTaskCommand)
+                const taskStatus = dataTask.SynthesisTask.taskStatus
+                
+                if (taskStatus == "completed") {
+                    const url = dataTask.SynthesisTask.OutputUri
+                    return url
+                } else if (taskStatus == "failed") {
+                    const response = "O trabalho de transcrição falhou ou foi cancelado."
+                    return response
+                } else {
+                    console.log("Ainda em andamento. Status:", taskStatus)
+                    await new Promise(resolve => setTimeout(resolve, 2000))
+                }
+            }
+        } catch (error) {
+            console.error("Erro inesperado", error)
+            return "Erro inesperado"
+        }
     }
 }
+
+module.exports = PollyService
