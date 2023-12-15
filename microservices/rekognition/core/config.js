2f2b7206ad17ce15dace7e73a40e02f2f7a820f2
luizscarsi <luizaugustoscarsi@gmail.com>
2023-12-15 11:18:50 -0300

    feat: Add rekognition .env config

---
+++ microservices/rekognition/core/config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+
+class Settings {
+  constructor() {
+    this.BUCKET_NAME = process.env.BUCKET_NAME;
+  }
+}
+
+module.exports = new Settings();
